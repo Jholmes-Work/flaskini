149edf2d9c7b02ee28f77a91d63710a5578770c5
jholmes16 <Jahmaul.Holmes@marist.edu>
2023-06-12 12:50:25 -0400

    add all previous files

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, jsonify, request, url_for, redirect, session, render_template
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+#Makes it so users can't modify session, should be radom (don't put sensentive info in session)
+app.config['SECRET_KEY'] = 'Thisisasecret!'
+
+@app.route('/')
+def index():
+    session.pop('name', None)
+    return '<h1>Hello World!</h1>'
+
+@app.route('/home', methods=['POST', 'GET'], defaults={'name' : 'default'})
+@app.route('/home/<string:name>', methods=['POST', 'GET'])
+def home(name):
+    session['name'] = name
+    return render_template('home.html', name=name, display=True, mylist=['one', 'two', 'three', 'four'], 
+                           listofdict=[{'name' : 'Zach'}, {'name': 'Zoey'}])
+
+@app.route('/json')
+def json():
+    if 'name' in session:
+        name = session['name']
+    else:
+        name = 'Not in session'
+    return jsonify({'key' : 'value', 'listkey' : [1,2,3], 'name': name})
+
+@app.route('/query')
+def query():
+    name = request.args.get('name')
+    location = request.args.get('location')
+    return '<h1>Hi {}.  You are from {}. You are on the query page!</h1>.'.format(name, location)
+
+@app.route('/theform', methods=['GET', 'POST'])
+def theform():
+    
+    if request.method == 'GET':
+        return  render_template('form.html')
+    else:
+        name = request.form['name']
+        #location= request.form['location']
+
+        #return 'Hello {}.  You are from {}.  You have submitted the form sucessfully!'.format(name, location)
+        return redirect(url_for('home', name=name))
+'''
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    location= request.form['location']
+
+    return 'Hello {}.  You are from {}.  You have submitted the form sucessfully!'.format(name, location)
+'''
+@app.route('/processjson', methods=['POST'])
+def processjson():
+
+    data = request.get_json()
+
+    name = data['name']
+    location = data['location']
+    randomlist = data['randomlist']
+
+    return jsonify({'result' : 'Success', 'name' : name, 'location' : location, 'randomkeyinlist' : randomlist[1]})
+
+
+if __name__ == '__main__':
+    app.run()
